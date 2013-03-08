94cc7dcb4b914bf30463550646bccc55cbff1359
John Basista <03bass23@gmail.com>
2013-03-08 14:10:24 -0500

    SDL

---
+++ SDL/Last_Known_Working/README.txt
@@ -0,0 +1,13 @@
+Must have SDL libraries and make installed before using this:
+
+	apt-get install libsdl1.2-dev libsdl-image1.2-dev automake
+
+Then run "make" command
+
+To use, ensure ADB connection has been established by running and ensuring device is recognized:
+
+	adb devices
+
+In order to run and display images on screen run and everything should be done for you:
+
+	./sdl_test
---
+++ SDL/Last_Known_Working/sdl_test.c
@@ -0,0 +1,82 @@
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+ 
+int main(int argc, char* argv[])
+{
+    SDL_Surface *screen = NULL;
+    SDL_Surface *image = NULL;
+    const SDL_VideoInfo *videoInfo = NULL;
+    
+    /*-----------------------------------------------------------------*/
+
+/*    if (argc != 2)
+    {
+        fprintf(stderr, "single argument ... name of image to display\n");
+        return 1;
+    }*/
+
+    /*-----------------------------------------------------------------*/
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        fprintf(stderr, "SDL_Init failed - %s\n", SDL_GetError());
+        return 1;
+    }
+
+    /*-----------------------------------------------------------------*/
+
+    videoInfo = SDL_GetVideoInfo();
+
+    if (videoInfo == 0)
+    {
+        fprintf(stderr, "SDL_GetVideoInfo failed - %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    /*-----------------------------------------------------------------*/
+
+	while(1){
+
+		/* Assuming that an ADB connection has already been made outside of this code, we can get screenshot from Android and save as screencap.png in this directory */
+		system("adb shell \"screencap -p | busybox uuencode -\" | uudecode > screencap.png");
+		
+		image = IMG_Load("screencap.png");
+
+		//image = SDL_DisplayFormat(image);
+
+		if (!image)
+		{
+			fprintf(stderr, "IMG_Load failed - %s\n", IMG_GetError());
+			SDL_Quit();
+			return 1;
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		screen = SDL_SetVideoMode(800,
+								  720,
+								  videoInfo->vfmt->BitsPerPixel,
+								  SDL_SWSURFACE);
+
+		if (!screen)
+		{
+			fprintf(stderr, "SetVideoMode failed - %s\n", SDL_GetError());
+			SDL_FreeSurface(image);
+			SDL_Quit();
+			return 1;
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		SDL_BlitSurface(image, 0, screen, 0);
+
+		//SDL_Flip(screen);
+
+		SDL_FreeSurface(image);
+	}
+
+    SDL_Quit();
+
+    return 0;
+}
